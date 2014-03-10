17ce936d75c0d0bba8766d1797961f5d1dff9663
Doug Zorn BE-111 <namelessone.doug@gmail.com>
2014-03-10 13:37:47 -0700

    revert

--- Slave/read_write.h
+++ Slave/read_write.h
@@ -2,30 +2,27 @@
 #ifndef READ_WRITE_H
 #define READ_WRITE_H
 
-char WriteReg(char addr, char value) //see page 22 of cc2500 data sheet for timing
+void WriteReg(char addr, char value) //see page 22 of cc2500 data sheet for timing
 {
-  char x;
   digitalWrite(SS,LOW);
   delayMicroseconds(150);
   while (digitalRead(MISO) == HIGH)
   {
   };    
-  x = SPI.transfer(addr);
+  SPI.transfer(addr);
   delayMicroseconds(1);
   SPI.transfer(value);
   delayMicroseconds(1);
   digitalWrite(SS,HIGH);
-  return x;
 }
 
-char WriteReg_burst(char addr, char value[], byte count)
+void WriteReg_burst(char addr, char value[], byte count)
 {
-  char x;
   digitalWrite(SS,LOW);
   delayMicroseconds(150);  
   while (digitalRead(MISO) == HIGH) {
   };
-  x = SPI.transfer(addr);
+  SPI.transfer(addr);
   delayMicroseconds(1);    
   for(byte i = 0; i<count; i++)
   {
@@ -33,7 +30,6 @@ char WriteReg_burst(char addr, char value[], byte count)
   }
   delayMicroseconds(1);
   digitalWrite(SS,HIGH);  
-  return x;
 }
 
 char ReadReg(char addr){
@@ -50,16 +46,13 @@ char ReadReg(char addr){
   return y;  
 }
 
-char SendStrobe(char strobe){
-  char x;
+void SendStrobe(char strobe){
   delayMicroseconds(150);
   digitalWrite(SS,LOW);  
   while (digitalRead(MISO) == HIGH) {
   };  
-  x = SPI.transfer(strobe);
+  SPI.transfer(strobe);
   delayMicroseconds(1); 
   digitalWrite(SS,HIGH);    
-  return x;
 }
 #endif
-
